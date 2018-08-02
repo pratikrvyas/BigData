68d59f9588d8f471b3f4d2e776207e4bde5ad9a5
Pratik <naukri.com.2014@gmail.com>
2018-08-02 20:21:02 +0400

    python code in new folder

---
+++ python-spark-tutorial/rdd/WordCount.py
@@ -0,0 +1,15 @@
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("word count").setMaster("local[3]")
+    sc = SparkContext(conf = conf)
+    
+    lines = sc.textFile("G:/ProgramFiles/GitRepo/pratikrvyas/Git/in/word_count.text")
+    
+    words = lines.flatMap(lambda line: line.split(" "))
+    
+    wordCounts = words.countByValue()
+    
+    for word, count in wordCounts.items():
+        print("{} : {}".format(word, count))
+
